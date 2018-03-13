14d30ccb0f605045a6ce329a5a2376845c761749
AmrObjection <amrobjection@AmrObjections-Mac.local>
2018-03-13 07:56:24 -0700

    side menu

--- AbadTradeApp/ViewController.swift
+++ AbadTradeApp/ViewController.swift
@@ -4,15 +4,22 @@
 //
 //  Created by AmrObjection on 3/13/18.
 //  Copyright © 2018 AmrObjection. All rights reserved.
-//
+
 
 import UIKit
+import SideMenu
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      SideMenuManager.default.menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? UISideMenuNavigationController
+        
+        // to control the slide menu by touch
+        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+
     }
 
     override func didReceiveMemoryWarning() {
