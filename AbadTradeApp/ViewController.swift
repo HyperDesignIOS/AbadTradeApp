13439486e516546d23e341ad556e8cb2e439f13b
Hyper Design <hyperdesign@hypers-imac.local>
2018-03-13 18:29:29 +0200

    add server manager , category and image models

--- AbadTradeApp/ViewController.swift
+++ AbadTradeApp/ViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 import SideMenu
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    
+    var categoriesArray = [Category]()
+    var imagesArray = [Image]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +32,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadCategoriesAndImages(){
+        let sm = serverManager()
+        sm.connectForApiWith(url: "http://hyper-design.com/Abad/api", mType: HTTPServerMethod.get, params: [:], complation: { (json) in
+            
+            if let obj = json {
+                print (obj)
+                let dictionaryOfJson = JSON(json!).dictionaryObject
+                let categorys = dictionaryOfJson!["category"] as! [[String : Any]]
+                for cat in categorys {
+                    let category = Category.init(fromDictionary: cat)
+                    self.categoriesArray.append(category)
+                    print(category.nameEn)
+                }
+                let images = dictionaryOfJson!["images"] as! [[String : Any]]
+                for img in images {
+                    let image = Image.init(fromDictionary: img)
+                    self.imagesArray.append(image)
+                    print(image.headerPhoto1)
+                }
+                
+            }
+        }, errorHandler: { (error, msg) in
+            print("\(msg!)")
+        })
+    }
+
 
 
 }
