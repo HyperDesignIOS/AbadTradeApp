58be9244f0d48dbbc53933c8799dc23ea3862915
HyperDesignIOS <gehadgamal045@gmail.com>
2018-03-18 14:14:57 +0200

    add search controllers again

---
+++ AbadTradeApp/Controllers/HomeControllers/searchTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  searchTableViewCell.swift
+//  AbadTradeApp
+//
+//  Created by Hyper Design on 3/18/18.
+//  Copyright © 2018 AmrObjection. All rights reserved.
+//
+
+import UIKit
+
+class searchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var searchResultLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
