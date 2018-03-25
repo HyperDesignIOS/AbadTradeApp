28d66774eaf12c7f12ff82a4aef51680ebbb9ba3
AmrObjection <>
2018-03-25 04:54:12 -0700

    insurance controller

---
+++ AbadTradeApp/Controllers/InsuranceCompany /InsuranceCompanyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  InsuranceCompanyCollectionViewCell.swift
+//  AbadTradeApp
+//
+//  Created by AmrObjection on 3/25/18.
+//  Copyright © 2018 AmrObjection. All rights reserved.
+//
+
+import UIKit
+
+class InsuranceCompanyCollectionViewCell: UICollectionViewCell {
+    
+}
