a6ffc04a0af41e2c19218c74791320d3f4188848
AmrObjection <>
2018-03-28 05:05:52 -0700

    modify controller name

--- AbadTradeApp/Controllers/Vehicles/VehiclesCollectionViewController.swift
+++ AbadTradeApp/Controllers/Vehicles/VehiclesCollectionViewController.swift
@@ -71,7 +71,7 @@ class VehiclesCollectionViewController: UICollectionViewController ,UICollection
             self.brands = brands
             
             let storyboard = UIStoryboard.init(name: "Vehicle", bundle: nil)
-            let destinationViewController = storyboard.instantiateViewController(withIdentifier: "BrandModelCollectionController") as! BrandModelCollectionViewController
+            let destinationViewController = storyboard.instantiateViewController(withIdentifier: "BrandModelCollectionController") as! BrandCollectionViewController
             
             destinationViewController.brands = brands
             self.show(destinationViewController, sender: self)
